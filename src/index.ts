191f35dac8204b1965198db6a171ac206c529701
ingridmsf19 <81247538+ingridmsf19@users.noreply.github.com>
2022-08-14 22:19:57 -0300

    implementando rotas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from "express";
+import statusRoute from "./routes/status.route";
+import userRoute from "./routes/users.route";
 
 const app = express();
 
-app.get("/status", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'bar'});
-});
+//Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//Configuração de Rotas
+app.use(userRoute);
+app.use(statusRoute);
+
+// Inicialialização do servidor
 app.listen(3000, () => {
-console.log('Aplicação executando na porta 3000!');
-})
+  console.log("Aplicação executando na porta 3000!");
+});
---
+++ src/routes/users.route.ts
@@ -0,0 +1,42 @@
+import { Router, NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const userRoute = Router();
+
+userRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
+   
+    const users = [{ userName: 'Ingrid'}];
+
+    res.status(StatusCodes.OK).send(users);
+});
+
+userRoute.get('/users/:uuid', (req: Request <{ uuid: string }>, res: Response, next: NextFunction) =>{
+
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+
+});
+
+userRoute.post('/users', (req: Request <{ uuid: string }>, res: Response, next: NextFunction) =>{
+    const newUser = req.body;
+
+    res.status(StatusCodes.CREATED).send(newUser);
+
+});
+
+userRoute.put("/users/:uuid",  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+
+    modifiedUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send( modifiedUser );
+});
+
+userRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+
+});
+
+export default userRoute;
